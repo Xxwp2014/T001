d5b278ae395c55a6f6209ecbfe5a0d1f2f8e8b47
Xxwp2014 <wp2014@foxmail.com>
2025-02-14 18:53:19 +0800

    Update 星空影院.js

--- js/星空影院.js
+++ js/星空影院.js
@@ -1,22 +1,22 @@
 
 var rule = {
 	title:'星空影院',
-	host:'https://corsproxy.bunkum.us/corsproxy/?apiurl=https://tedy.cc',
-	//homeUrl:'/tedy',
-	url: '/tedy/fyclass-fypage',
+	host:'https://corsproxy.bunkum.us',
+	homeUrl:'/corsproxy/?apiurl=https://tedy.cc',
+	url: '/corsproxy/?apiurl=https://tedy.cc/tedy/fyclass-fypage',
 	//homeUrl:'https://api.web.360kan.com/v1/rank?cat=2&size=9',
     //detailUrl:'https://api.web.360kan.com/v1/detail?cat=fyclass&id=fyid',
     //searchUrl:'https://api.so.360kan.com/index?force_v=1&kw=**&from=&pageno=fypage&v_ap=1&tab=all',
     //url:'https://api.web.360kan.com/v1/filter/list?catid=fyclass&rank=rankhot&cat=&year=&area=&act=&size=35&pageno=fypage&callback=',
 	
-	searchUrl: '/search/?wd=**',
+	searchUrl: '/corsproxy/?apiurl=https://tedy.cc/tedy/search/?wd=**',
 	searchable:2,
 	quickSearch:0,
 	filterable:1,
 	headers:{
 		'User-Agent': 'MOBILE_UA Android AppleWebKit Mobile'
 	},
-	class_name:'电视剧&综艺A&电影',
+	class_name:'电视剧&综艺B&电影',
     class_url:'dianshiju&zhongyi&dianying',
 	timeout:5000,
 	play_parse:true,	
